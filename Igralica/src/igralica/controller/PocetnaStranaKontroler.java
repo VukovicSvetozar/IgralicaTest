86f73659e1c3a4efb80930d745463071f48f938d
vukovicsvetozar <vukovicsvetozar@gmail.com>
2018-12-14 17:19:50 +0100

    Početna strana aplikacije

---
+++ Igralica/src/igralica/controller/PocetnaStranaKontroler.java
@@ -0,0 +1,37 @@
+package igralica.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class PocetnaStranaKontroler {
+
+	@FXML
+	private Label lblStatus;
+
+	@FXML
+	private TextField txtKorisnickoIme;
+
+	@FXML
+	private PasswordField txtLozinka;
+
+	@FXML
+	private Button btnPrijava;
+
+	@FXML
+	private Button btnRegistracija;
+
+	@FXML
+	void Prijava(ActionEvent event) {
+
+	}
+
+	@FXML
+	void Registracija(ActionEvent event) {
+
+	}
+
+}
